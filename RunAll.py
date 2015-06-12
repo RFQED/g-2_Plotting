e4bae6380bee42f46adc312ec48220e8ad085e54
RFQED <willturner1105@yahoo.co.uk>
2015-06-12 12:08:55 -0500

    added script to Run All

---
+++ RunAll.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("python mtestRecoAnalysis_SiliconRecoPlots.py")
+os.system("python mtestReadoutAnalysis_strawTruthSanityPlots.py")
+os.system("python mtestReadoutAnalysis_strawReadoutSanityPlots.py")
+os.system("python mtestReadoutAnalysis_siliconDigitPlots.py")
+os.system("python mtestRawAnalysis.py")
+os.system("python mtestGunAnalysis_strawTruthSanityPlots.py")
+os.system("python mtestGunAnalysis_siliconTruthSanityPlots.py")
+
+print "##### All Done #####"
+
